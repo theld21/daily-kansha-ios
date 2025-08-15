bfbf8cb82e5dae10c8ab082ffc837d8c35111a3c
theld21 <thedev.ldt@gmail.com>
2025-08-15 20:53:51 +0700

    Initial Commit

---
+++ daily kansha/daily_kanshaApp.swift
@@ -0,0 +1,17 @@
+//
+//  daily_kanshaApp.swift
+//  daily kansha
+//
+//  Created by ldt on 15/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct daily_kanshaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
